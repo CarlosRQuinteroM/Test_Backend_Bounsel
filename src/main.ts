65c5d02faa3dffb46ab7f55b138c5b0d7f19d319
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-08-04 14:27:21 +0200

    Installing dependencies and creating a working environment with Nestjs

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {ConfigService} from '@nestjs/config'
+import { SERVER_PORT } from './config/constents';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const consigService = app.get(ConfigService);
+
+  const port = +consigService.get<number>(SERVER_PORT) || 3000;
+  await app.listen(port);
+  console.log(`listening on port ${await app.getUrl()}`)
+}
+bootstrap();
